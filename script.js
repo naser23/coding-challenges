93a4fb614908c376f32297f72b801da96ccc1f9f
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-12-12 09:01:33 -0500

    reviewing prototypal inheritance and prototype chain.

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ console.log(jonas.__proto__ === Person.prototype); // true
 Person.prototype.species = "Homo Sapiens";
 console.log(jonas.species, matilda.species);
 
-console.log(jonas.hasOwnProperty("firstName"));
-console.log(jonas.hasOwnProperty("species"));
+// console.log(jonas.hasOwnProperty("firstName"));
+// console.log(jonas.hasOwnProperty("species"));
 
 // when an object is created with a constructor function or ES6 classes:
 
@@ -54,3 +54,29 @@ console.log(jonas.hasOwnProperty("species"));
 // 2. (this) keyword in constructor function call is set to the new object. (this = {})
 // 3. new {} is linked to the constructor functions prototype.
 // 4. the new {} is always returned unless you explicitly stated otherwise.
+
+console.log(jonas.__proto__); // Person.prototype
+console.log(jonas.__proto__.__proto__); // Object.prototype
+console.log(jonas.__proto__.__proto__.__proto__); // null
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 9, 8, 3, 5, 6]; // new Array === []
+console.log(arr.__proto__ === Array.prototype); // true
+console.log(arr.__proto__.__proto__);
+
+// REMEMBER: (arr.__proto__) is an object. so it has a prototype. here: arr.__proto__.__proto__;
+
+// DON'T DO THIS
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+// console.log(arr);
+
+const h1 = document.querySelector("h1");
+console.dir(h1);
+console.dir((x) => x + 1);
+console.dir(Person.prototype);
+console.dir(jonas.__proto__);
