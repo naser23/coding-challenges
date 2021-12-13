0d6bb6915515f4a4c9cbcc5f4a73f18fbbce9bbb
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-12-13 11:11:37 -0500

    did a coding challenge to test my knowledge of constructor functions and prototypal inheritance so far.

--- script.js
+++ script.js
@@ -32,18 +32,18 @@ Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-jonas.calcAge();
-matilda.calcAge();
-jack.calcAge();
+// jonas.calcAge();
+// matilda.calcAge();
+// jack.calcAge();
 
 // Person.prototype IS NOT the prototype of Person. it is essentailly the same prototype as the created objects (that will be created using the prototype of the constructor function). Think of the prototype keyword as saying (.prototypeOfLinkedObjects). refer to below:
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype); // true
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype); // true
 
-Person.prototype.species = "Homo Sapiens";
-console.log(jonas.species, matilda.species);
+// Person.prototype.species = "Homo Sapiens";
+// console.log(jonas.species, matilda.species);
 
 // console.log(jonas.hasOwnProperty("firstName"));
 // console.log(jonas.hasOwnProperty("species"));
@@ -55,15 +55,15 @@ console.log(jonas.species, matilda.species);
 // 3. new {} is linked to the constructor functions prototype.
 // 4. the new {} is always returned unless you explicitly stated otherwise.
 
-console.log(jonas.__proto__); // Person.prototype
-console.log(jonas.__proto__.__proto__); // Object.prototype
-console.log(jonas.__proto__.__proto__.__proto__); // null
+// console.log(jonas.__proto__); // Person.prototype
+// console.log(jonas.__proto__.__proto__); // Object.prototype
+// console.log(jonas.__proto__.__proto__.__proto__); // null
 
-console.dir(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor);
 
-const arr = [3, 6, 4, 5, 9, 8, 3, 5, 6]; // new Array === []
-console.log(arr.__proto__ === Array.prototype); // true
-console.log(arr.__proto__.__proto__);
+// const arr = [3, 6, 4, 5, 9, 8, 3, 5, 6]; // new Array === []
+// console.log(arr.__proto__ === Array.prototype); // true
+// console.log(arr.__proto__.__proto__);
 
 // REMEMBER: (arr.__proto__) is an object. so it has a prototype. here: arr.__proto__.__proto__;
 
@@ -75,8 +75,37 @@ console.log(arr.__proto__.__proto__);
 // console.log(arr.unique());
 // console.log(arr);
 
-const h1 = document.querySelector("h1");
-console.dir(h1);
-console.dir((x) => x + 1);
-console.dir(Person.prototype);
-console.dir(jonas.__proto__);
+// const h1 = document.querySelector("h1");
+// console.dir(h1);
+// console.dir((x) => x + 1);
+// console.dir(Person.prototype);
+// console.dir(jonas.__proto__);
+
+// Coding Challenge #1
+
+function Car(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+console.log(Car.prototype);
+
+let bMW = new Car("BMW", 120);
+let mercedes = new Car("Mercedes", 95);
+
+bMW.accelerate();
+mercedes.accelerate();
+
+bMW.break();
+mercedes.break();
+
+console.log(bMW, mercedes);
